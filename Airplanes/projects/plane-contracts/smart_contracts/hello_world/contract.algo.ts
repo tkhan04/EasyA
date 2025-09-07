0c7e99d9612624d09de7cca3d8c2b30cebcf3404
tkhan04 <tkhanpsn@gmail.com>
2025-09-06 22:02:04 -0400

    name update

--- Airplanes/projects/plane-contracts/smart_contracts/hello_world/contract.algo.ts
+++ Airplanes/projects/plane-contracts/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,7 @@
 import { Contract } from '@algorandfoundation/algorand-typescript'
 
 export class HelloWorld extends Contract {
-  public hello(name: string): string {
-    return `Hello, ${name}`
+  public hello(owner: string, shoeName: string): string {
+    return `Hello, ${owner} ${shoeName}`
   }
 }
